42263a1beb97edc6efd62c9ccc985c5e91d619d7
Cardelli <felipecardelli@gmail.com>
2018-03-22 08:19:12 -0300

    21/03/2018

--- JavaScript/Exercicios-JS/objeto/objetoRevisao.js
+++ JavaScript/Exercicios-JS/objeto/objetoRevisao.js
@@ -35,11 +35,10 @@ const carro = {
 carro.proprietario.endereco.numero = 1000
 carro['proprietario']['endereco']['logradouro'] = 'Av Gigante'
 console.log(carro)
-/*
+
 delete carro.condutores
 delete carro.proprietario.endereco
 delete carro.calcularValorSeguro
 console.log(carro)
 console.log(carro.condutores)
-*/
-4
+//console.log(carro.condutores.length)
