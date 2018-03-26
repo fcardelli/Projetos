e433bf6facf83274f5cf06fd342cbc5c9cd8113a
Cardelli <felipecardelli@gmail.com>
2018-03-26 17:01:01 -0300

    26/03/2018

--- JavaScript/Exercicios-JS/exerciciosprova/registraUsuario.js
+++ JavaScript/Exercicios-JS/exerciciosprova/registraUsuario.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
-    $("submit").click(function(){
-        let primeiroNome = document.getElementById('primeiroNome').value
-        let ultimoNome = document.getElementById('ultimoNome').value
-
-        document.getElementById('labelResposta').innerHTML = primeiroNome + '' + ultimoNome
+    $("button").click(function(){
+        let primeiroNome = document.getElementById("primeiroNome").value
+        let ultimoNome = document.getElementById("ultimoNome").value
+        
+        $("#labelResposta").value = primeiroNome + '' + ultimoNome
     });
 });
